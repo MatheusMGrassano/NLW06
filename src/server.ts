35df4ce37fb9e649f073673faea76b6fa27cc9c9
MatheusMGrassano <grassanomatheus@gmail.com>
2021-06-21 23:59:33 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/test", (request, response) => {
+    return response.send("Ola NLW")
+});
+
+app.post("/test-port", (request, response) => {
+    return response.send("Ola NLW metodo POST")
+})
+
+// http://localhost:3000
+app.listen(port, () => console.log(`🦁 Server is running on port ${port}`) )
