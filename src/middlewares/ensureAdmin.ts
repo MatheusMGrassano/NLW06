6d98b928473b0df16d46cb639ba052cd70951eb4
MatheusMGrassano <grassanomatheus@gmail.com>
2021-06-24 16:55:58 -0300

    Tag creation

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction, request, response } from "express"
+
+
+
+export function ensureAdmin(request: Request, response: Response, next: NextFunction){
+
+    const admin = false;
+
+    if(admin){
+        return next();
+    }
+
+    return response.status(401).json({
+        error: "Permission denied"
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response, NextFunction, response } from "express";
 import "reflect-metadata";
 import "./database";
+import "express-async-errors";
 
 import { router } from "./routes";
 
@@ -10,6 +11,19 @@ const port = 3000;
 app.use(express.json());
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+}) 
+
 
 // http://localhost:3000
 app.listen(port, () => console.log(`🦁 Server is running. Port ${port}`) )
